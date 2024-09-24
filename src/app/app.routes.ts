07c6c78942e7f391633ee783bfd4cfde7204f06b
Somnath Bera <SomnathB@interrait.com>
2024-09-24 18:20:42 +0530

    route config, redircting to contact list from home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { ContactListComponent } from './components/contact-list/contact-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/contacts', pathMatch: 'full' },
+    { path: 'contacts', component: ContactListComponent }
+  ];
+
+
